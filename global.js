576a030c1f130eb850e4640ab30634e55984a8b6
BrianDoce <docena.brian@gmail.com>
2025-01-24 07:15:58 -0800

    Fixed selected color theme

--- global.js
+++ global.js
@@ -4,10 +4,66 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
+// let navLinks = $$("nav a");
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname
-  );
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+//   );
 
-currentLink?.classList.add('current');
+// currentLink?.classList.add('current');
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'Resume/', title: 'Resume'},
+    { url: 'contact/', title: 'Contact'},
+    { url: 'https://github.com/BrianDoce', title: 'GitHub'}
+
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+for (let p of pages) {
+    let url = p.url;
+    
+    if (!ARE_WE_HOME && !url.startsWith('http')) {
+        url = '../' + url;
+    }
+    
+    let title = p.title;
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+
+    if (a.host !== location.host) {
+        a.target = "_blank";
+    }
+}
+
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+      <label class="color-scheme">
+          Theme:
+          <select>
+                <option>Automatic</option>
+                <option>Light</option>
+                <option>Dark</option>
+          </select>
+      </label>`
+);
+
+const select = document.querySelector(".color-scheme");
+
+select.addEventListener('input', function (event) {
+    console.log('color scheme changed to', event.target.value);
+    document.documentElement.style.setProperty('color-scheme', event.target.value);
+});
